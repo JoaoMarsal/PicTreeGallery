11663bf95bbe55552d11e27aee4121613706003d
JoaoMarsal <98599658+JoaoMarsal@users.noreply.github.com>
2025-10-12 08:29:44 -0400

    loginCSS
    worked on CSS for the login and register page

--- C/login.php
+++ C/login.php
@@ -2,5 +2,8 @@
     $userName = $_POST['nName'];
     $userPass = $_POST['nPass'];
 
+    session_start();
+    require_once '../M/config.php';
     
+
 ?>
--- V/index.php
+++ V/index.php
@@ -7,13 +7,26 @@
     <link rel="stylesheet" href="index.css">
 </head>
 <body>
-    <div id="login">
-        <h1 class="title">Login</h1>
-        <form name="nForm" method="POST" action="../C/login.php">
-            <input type="text" name="nName" placeholder="User">
-            <input type="password" name="nPass" placeholder="Password">
-            <input type="submit" value="Get in">
-        </form>
-    </div>    
+    <section class="boxes">
+        <div id="login" class="divForm">
+            <h1 class="title">Login</h1>
+            <form name="nForm" method="POST" action="../C/login.php">
+                <input class="formInput" type="text" name="nName" placeholder="User">
+                <input class="formInput" type="password" name="nPass" placeholder="Password">
+                <input type="submit" value="Get in" class="button">
+            </form>
+            <p>Don't have an account? <a>Sign up</a></p>
+        </div>    
+
+        <div id="register" class="divForm">
+            <h1 class="title">Registro</h1>
+            <form name="nRegister">
+                <input class="formInput" type="text" placeholder="User name">
+                <input class="formInput" type="text" placeholder="E-mail">
+                <input class="formInput" type="password" placeholder="Password">
+                <input type="submit" value="Sign up" class="button">
+            </form>
+        </div>
+    </section>
 </body>
 </html>
