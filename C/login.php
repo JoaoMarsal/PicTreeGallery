d3f26b812ac23d16a3b7569ed5684bd06c0cc1af
JoaoMarsal <98599658+JoaoMarsal@users.noreply.github.com>
2025-10-14 21:35:09 -0400

    connectDatabase

--- C/login.php
+++ C/login.php
@@ -5,5 +5,13 @@
     session_start();
     require_once '../M/config.php';
     
-
+    if(isset($_POST['register']))   {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+        
+        $checkEmail = $conn->query("SELECT email FROM users WHERE email = '$email'"){
+            
+        }
+    }
 ?>
