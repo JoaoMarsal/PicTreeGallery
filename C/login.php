ad384c93c85e5fc4a532b9b26bbd375b4e865841
JoaoMarsal <98599658+JoaoMarsal@users.noreply.github.com>
2025-10-26 15:38:24 -0400

    userTypeRedirection
    login system will redirect user based on usertype.

--- C/login.php
+++ C/login.php
@@ -23,14 +23,23 @@
 
     if(isset($_POST["nLogin"])){
         $name = $_POST['nName'];
-        $password = password_hash($_POST["nPassword"], PASSWORD_DEFAULT);
+        $password = $_POST['nPassword'];
 
         $checkUser = $conn->query("SELECT * FROM users WHERE name = '$name' AND password = '$password'");
-        if($checkUser->num_rows == 0){
+        if($checkUser->num_rows > 0){
+            $user = $checkUser->fetch_assoc();
+            if(password_verify($password, $user['name'])){
+                $_SESSION['name'] = $user['name'];
+                $_SESSION['email'] = $user['email'];
+                if($user['role'] === 'admin'){
+                    header('Location: admin_page.php');
+                } else {
+                    header('Location: user_page.php');
+                }
+            }
+        } else {
             $_SESSION['login_malfunction'] = "Account doesn't exists";
             $_SESSION['active_form'] = "nLogin";
-        } else {
-            //Password_verification
         }
     }
 ?>
