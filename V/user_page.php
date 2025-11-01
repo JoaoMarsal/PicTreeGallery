ddc78ff23e32c66230395c79c4d434142678f620
JoaoMarsal <98599658+JoaoMarsal@users.noreply.github.com>
2025-11-01 11:41:48 -0400

    userPageDataFetch
    The user page will consult the session global

--- V/user_page.php
+++ V/user_page.php
@@ -1,3 +1,20 @@
 <?php
-    echo 'oi, ';
+    session_start();
+
+    $user = $_SESSION['name'];
+    $email = $_SESSION['email']
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PicTree - Your place for photos</title>
+    <?php
+        echo 'Oi ', $user; 
+    ?>
+</head>
+<body>
+    
+</body>
+</html>
