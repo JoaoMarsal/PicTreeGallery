810c291ff2224d350458f7ab2c8c664893d6ddaa
JoaoMarsal <98599658+JoaoMarsal@users.noreply.github.com>
2025-11-01 12:44:43 -0400

    Navbar
    worked on navbar of userpage

--- V/user_page.php
+++ V/user_page.php
@@ -5,16 +5,25 @@
     $email = $_SESSION['email']
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/user.css">
     <title>PicTree - Your place for photos</title>
-    <?php
-        echo 'Oi ', $user; 
-    ?>
 </head>
 <body>
-    
+    <header>
+        <nav id="navbar">
+                <a class="textfornav">Home</a>
+                <a class="textfornav">Explore</a>
+                <a class="textfornav">Me</a>
+                <a class="textfornav">About us</a>
+                <a class="textfornav">Support</a>
+        </nav>
+    </header>
+    <session></session>
+    <session></session>
+    <footer></footer>
 </body>
 </html>
