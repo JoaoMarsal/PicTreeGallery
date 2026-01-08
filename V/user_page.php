10b7dc37d30756fec85448e1c97b1f2b3c8283ec
JoaoMarsal <98599658+JoaoMarsal@users.noreply.github.com>
2026-01-08 17:59:45 -0400

    imgRepositoryVisualization
    Setting out the plans for a quick slideshow of images users published on the website

--- V/user_page.php
+++ V/user_page.php
@@ -24,6 +24,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style/user.css">
+    <link rel="stylesheet" href="style/userGallery.css">
     <link href="https://fonts.googleapis.com/css2?family=BBH+Hegarty&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Google+Sans:ital,opsz,wght@0,17..18,400..700;1,17..18,400..700&display=swap" rel="stylesheet">
     <title>PicTree - Your place for photos</title>
@@ -68,7 +69,16 @@
                 </form>
             </div>
         </session>
-        <session id="photos"></session>
+        
+        <session id="photos">
+            <div>
+                <hr id="line">
+                <h1 id="title">Explore more pictures</h1>
+            </div>  
+            <div id="imgRepository">
+                
+            </div>             
+        </session>
     </div>
     <footer></footer>
     <script src="js/user.js">
