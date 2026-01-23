86fb592f07a68a3a265649d713438c69a16dd9ec
JoaoMarsal <98599658+JoaoMarsal@users.noreply.github.com>
2026-01-23 10:16:52 -0400

    quickCSS

--- V/Me/user_gallery.php
+++ V/Me/user_gallery.php
@@ -64,7 +64,7 @@
                             </select>
                         </div>
                         <div id="searchBox">
-                            <img src="https://cdn-icons-png.flaticon.com/512/54/54481.png" width="10px">
+                            <img id="lupa" src="https://cdn-icons-png.flaticon.com/512/54/54481.png" width="10px">
                             <input type="text" id="nameSearch" placeholder="Filtrar por nome..." onkeyup="callImages()">
                         </div>
                     </div>
