e67f591a15a34dc9e2eb35a62a22b84fa0eadebc
JoaoMarsal <98599658+JoaoMarsal@users.noreply.github.com>
2026-02-02 20:01:18 -0400

    supportEmail
    added form that helps users send emails.

--- V/support.php
+++ V/support.php
@@ -20,6 +20,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../style/user.css">
     <link rel="stylesheet" href="../style/userGallery.css">
+    <link rel="stylesheet" href="style/support.css">
     <link href="https://fonts.googleapis.com/css2?family=BBH+Hegarty&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Google+Sans:ital,opsz,wght@0,17..18,400..700;1,17..18,400..700&display=swap" rel="stylesheet">
     <title>PicTree - Your place for photos</title>
@@ -34,6 +35,28 @@
                  <h1 id="title">Support</h1>
             </div>
         </session>
+        <session id="textSess"> 
+            <div class="textBox">
+                <p>Hello, welcome to the support page! A type of feedback and helping system of the PicTree website!</p>
+            </div>
+        </session>
+        <div id="emailSess">
+            <form action="mailto:ja.mv09052007@gmail.com" method="post" enctype="text/plain">
+                <div>
+                    <label for="name">Name: </label>
+                    <input name="name" id="name" type="text">
+                </div>
+                <div>
+                    <label for="email">Email: </label>
+                    <input name="email" id="email" type="text">
+                </div>   
+                <div>
+                    <label for="message">Message:</label>
+                    <textarea name="comment" id="message"></textarea>
+                </div> 
+                <input type="submit" value="Send">
+            </form>
+        </div>      
     </div>
 </body>
 </html>
